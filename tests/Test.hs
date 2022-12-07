353f71af9a508f10c17782c4af39a2467a11524d
Diean233 <wdxiop@outlook.com>
2022-12-07 11:15:45 -0800

    dead tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -14,13 +14,13 @@ main :: IO ()
 main = do
   putStrLn "\nRunning my tests... "
   basicG <- initGame
-  runTests [probCore basicG]
+  runTests [probCore basicG,
+            probEnd  basicG]
   putStrLn "\nDone Testing"
 
 --exitWith ExitSuccess
 
 probCore :: Game -> Score -> TestTree
--- <<<<<<< HEAD
 probCore g sc =
   testGroup
     "MoveCore"
@@ -40,26 +40,14 @@ probCore g sc =
     jump1 :: Game -> IO Int
     jump1 g@(Game rn obs flg jump score lock dead) = return jump
 -- =======
--- probCore g sc = testGroup "MoveCore" [
---   scoreTest (move1, (moveUp g), 7, 1, "Up1")
---   -- scoreTest ((\_ -> checkImage "chess1.png"    mkChess1),     (), True, 2, "chess-1"),
---   -- scoreTest ((\_ -> checkImage "chess2.png"    mkChess2),     (), True, 2, "chess-2"),
---   -- scoreTest ((\_ -> checkImage "triangle1.png" mkTriangle1),  (), True, 3, "triangle-1"),
---   -- scoreTest ((\_ -> checkImage "triangle2.png" mkTriangle2),  (), True, 3, "triangle-2"),
---   -- scoreTest ((\_ -> checkImage "carpet.png"    mkCarpet),     (), True, 5, "carpet")
---   ]
---   where 
---     scoreTest :: (Show b, Eq b) => (a -> IO b, a, b, Int, String) -> TestTree
---     scoreTest (f, x, r, n, msg) = scoreTest' sc (f, x, r, n, msg)
---     move1 :: Game -> IO Int
---     move1 g@(Game rn obs flg jump score lock dead) = return $ trackCord(rn)
--- >>>>>>> 0b078a3c32f80619cf4fe39aa0be23a61a6038e2
-
--- basicG = Game { _Runner = Mid, -- current track (location) of runner
---     _Obstacles :: [], -- list of obstacles
---     _Flags :: Stream Int, --- list of 0/1 as flag to know if the obstacle need to be generate
---     _Jumping :: Int, -- is runner jumping now
---     _score :: Int, -- current score, method TODO
---     _locked :: Bool, -- game locked during moving/jumping
---     _dead :: Bool -- game over
---   }
+probEnd g sc =
+  testGroup
+    "EndCore"
+    [ scoreTest (end1, (step $ g {_Obstacles = [(Mid,0)]}), True, 1, "Up1"), 
+      scoreTest (end1, (step $ g {_Runner = Up,_Obstacles = [(Up,0)]}), True, 1, "Up1")
+    ]
+  where
+    scoreTest :: (Show b, Eq b) => (a -> IO b, a, b, Int, String) -> TestTree
+    scoreTest (f, x, r, n, msg) = scoreTest' sc (f, x, r, n, msg)
+    end1 :: Game -> IO Bool
+    end1 g@(Game rn obs flg jump score lock dead) = return $ dead
